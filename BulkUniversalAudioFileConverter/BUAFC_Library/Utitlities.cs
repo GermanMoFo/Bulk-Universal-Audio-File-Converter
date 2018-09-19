6d23f1894f9e030c8adcaf0b3827f9127c50a87b
forthelols69420 <forthelols69420@gmail.com>
2018-09-19 15:51:06 -0400

    - Established extension-filtering when choosing many files - began to lay down support for destination file redirection

---
+++ BulkUniversalAudioFileConverter/BUAFC_Library/Utitlities.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUAFC_Library
+{
+    static class Utitlities
+    {
+        public static string FindCommonPath(string Separator, IEnumerable<string> Paths)
+        {
+            string CommonPath = String.Empty;
+            List<string> SeparatedPath = Paths
+                .First(str => str.Length == Paths.Max(st2 => st2.Length))
+                .Split(new string[] { Separator }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            foreach (string PathSegment in SeparatedPath.AsEnumerable())
+            {
+                if (CommonPath.Length == 0 && Paths.All(str => str.StartsWith(PathSegment)))
+                {
+                    CommonPath = PathSegment;
+                }
+                else if (Paths.All(str => str.StartsWith(CommonPath + Separator + PathSegment)))
+                {
+                    CommonPath += Separator + PathSegment;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return CommonPath;
+        }
+    }
+}
